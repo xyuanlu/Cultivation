2d4ad459919dc172dfa735967aa6e8937fae94f1
xyuanlu <xyuanlu@gmail.com>
2019-10-15 03:26:43 -0700

    542. 01 Matrix -v2

--- 542.cpp
+++ 542.cpp
@@ -16,8 +16,47 @@ public:
         P(int xx, int yy, int dd) : x(xx), y(yy), d(dd) {};
     };
     
+    
+   // Similar to 361. Bomb Enemy
+   vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int M = matrix.size(), N = matrix[0].size();
+        vector<vector<int>> m(M, vector<int>(N, 100000000));
+        //vector<vector<int>> m2(M, vector<int>(N, 100000000));
+       
+        for (int i=0; i<M; ++i) {
+            for (int j=0; j<N; ++j) {
+                if (matrix[i][j] ==0) {
+                    m[i][j] =0;
+                } else {
+                    if (i>0)
+                        m[i][j] = min(m[i][j], m[i-1][j]+1);
+                    if (j>0)
+                         m[i][j] = min(m[i][j], m[i][j-1]+1);
+                }
+            }
+        }
+       
+       for (int i=M-1; i>=0; --i) {
+            for (int j=N-1; j>=0; --j) {
+                if (matrix[i][j] == 0) {
+                    m[i][j] =0;
+                } else {
+                    if (i<M-1)
+                        m[i][j] = min(m[i][j], m[i+1][j]+1);
+                    if (j<N-1)
+                        m[i][j] = min(m[i][j], m[i][j+1]+1);
+                }
+                //m1[i][j] = min(m1[i][j], m2[i][j]);
+            }
+        }
+       
+       return m;
+        
+    }
+    
+    
     // Similar to "317 shortest distance from all building"
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+    vector<vector<int>> updateMatrix2(vector<vector<int>>& matrix) {
         int M = matrix.size(), N = matrix[0].size();
         vector<vector<int>> res(M, vector<int>(N, INT_MAX));
         vector<vector<int>> visited(M, vector<int>(N, false));
@@ -40,16 +79,21 @@ public:
         while(!q.empty()) {
             P t = q.front();
             q.pop();
-            visited[t.x][t.y] = true;
-            if (matrix[t.x][t.y] == 1) {
-                res[t.x][t.y] = min(res[t.x][t.y], t.d);   // <<<<<-------
-            }
+            
+            // If we are pooping level by level (adding new element to qq and swap when q is empty)
+            // It is ok to set the visited here and update res. 
+            // Since res need to be updated level by level.
+            // Thus, we do it in the for loop since we only have one queue
+            
             int xx, yy;
             for (int k=0; k<4; ++k) {
                 xx = t.x+dx[k];
                 yy = t.y+dy[k];
                 if (xx>=0 && yy>=0 && xx<M && yy<N && !visited[xx][yy]) {
                     //cout<<xx<<" "<<yy<<endl;
+                    visited[xx][yy] = true;
+                    res[xx][yy] = t.d+1; //min(res[t.x][t.y], t.d);   // <<<<<-------
+                    
                     q.push(P(xx, yy, t.d+1));
                 }
             }
