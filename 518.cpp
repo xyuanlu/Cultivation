eecbbbbfb8b785e402076a57fca554af6fbef650
xyuanlu <xyuanlu@gmail.com>
2019-11-23 17:01:43 -0800

    Update 518. Coin Change 2

--- 518.cpp
+++ 518.cpp
@@ -4,23 +4,32 @@
  * You are given coins of different denominations and a total amount of money. Write a
  * function to compute the number of combinations that make up that amount. You may assume
  * that you have infinite number of each kind of coin.
- *
+ */
  
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         // m[i] means number of ways to get to i
         vector<int> m(amount+1, 0);
-        
         m[0]=1;
-        for(int j=0; j<coins.size(); ++j) {       // <<<<------------ Having coins or amount on topThis maters a lot.
-            for(int i=0; i<=amount; ++i) {        // Coins on top garentee unique ways.
-                int n = i+coins[j];  
+        
+        for(int i=0; i<coins.size(); ++i) {
+            for(int c=coins[i]; c<=amount; ++c) {
+                m[c] += m[c-coins[i]];
+            }
+        }
+        return m[amount];
+        
+        /*
+        TLE
+        for(int j=0; j<coins.size(); ++j) {
+            for(int i=0; i<=amount; ++i) {
+                int n = i+coins[j];
                 if (n<=amount) {
                     m[n] += m[i];
                 }
             }
         }
-        return m[amount];
+        return m[amount];*/
     }
 };
