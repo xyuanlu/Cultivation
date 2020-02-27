af59c8cad519062c9b8edfa72af9f1c76a97ef3c
xyuanlu <xyuanlu@gmail.com>
2020-02-27 12:30:19 -0800

    301 Remove Invalid Parentheses -- Update

--- 301.cpp
+++ 301.cpp
@@ -1,10 +1,71 @@
-class Solution {
+
     /*
      *  301 Remove Invalid Parentheses
      *  Backtracking + Pruning
      *  For each Parentheses,we can add or not add this to result. 
      *  We visit all possibilities and add valid result to res.
      */
+    
+
+// 4ms
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        // cound the number of left and right to be removed
+        int l = 0, r = 0;
+        for(char c:s) {
+            if (c == '(') {
+                ++l;
+            }
+            if (c == ')') {
+                if(l>0) --l;
+                else ++r;
+            }
+        }
+        
+        vector<string> res;
+        string temp;
+        //cout<<"##"<<l<<" "<<r<<endl;
+        helper(res, s, temp, 0, l, r, 0/*current left*/, 0/*current right*/);
+        return res;
+    }
+    
+    void helper(vector<string>& res, const string& s, string temp, int idx,
+                int trl /*to be removed left*/, int trr, int l/*cur left*/, int r) {
+        
+        //cout<<l<<" "<<r<<endl;
+        if (r>l) return;
+        if (idx == s.size()) {
+            //cout<<temp<<" ";
+            //cout<<trl<<" "<<trr<<endl;
+            if (trl==0 && trr==0 && l==r)
+                res.push_back(temp);
+            return;
+        }
+        if (s[idx] !='(' && s[idx] !=')') {
+            helper(res, s, temp+s[idx], idx+1, trl, trr, l, r);
+        }
+        if (s[idx] == '(') {
+            // do not remove if prev is not removed
+            helper(res, s, temp+s[idx], idx+1, trl, trr, l+1, r);
+            if (trl>0 && (idx==0 || temp.back()!='(')) {
+                helper(res, s, temp, idx+1, trl-1, trr, l, r);
+            }
+        }
+        if (s[idx] == ')') {
+            // do not remove if prev is not removed
+            helper(res, s, temp+s[idx], idx+1, trl, trr, l, r+1);
+            if (trr>0 && (idx==0 || temp.back()!=')')) {
+                helper(res, s, temp, idx+1, trl, trr-1, l, r);
+            }
+        }
+        
+        
+    }
+};
+    
+/* 72 ms
+class Solution {    
 public:
     int min = INT_MAX;
     vector<string> removeInvalidParentheses(string s) {
@@ -18,11 +79,10 @@ public:
     void helper(const string& s, vector<string>& res, set<string>& visited, int cur, string temp) {
         int l=0, r=0;
         isValid(temp, l, r);
-        /*
-         * Pruning:
-         * 1. If right >left (temp is not valid)
-         * 2. If the total number of removed parentheses in temo is already larger than removed parentheses in the global optimal.
-         */
+         // Pruning:
+         // 1. If right >left (temp is not valid)
+         // 2. If the total number of removed parentheses in temo is already larger than removed parentheses in the global optimal.
+         
         if(visited.find(temp) != visited.end() || r>l || cur - temp.size() >min)
             return;
         if(cur == s.size()) {
@@ -55,3 +115,4 @@ public:
         return l == r;
     }
 };
+*/
